508cb03cdee716d7d641a1dff7bb8851669208f2
Rivaldo Ramos <ramosmendesfilho@hotmail.com>
2023-02-20 16:50:38 -0300

    Classe Scanner adicionada

--- src/main/Program.java
+++ src/main/Program.java
@@ -1,16 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main;
 
-/**
- *
- * @author rivaldor
- */
+import java.util.Scanner;
+
 public class Program {
 
-    public static void main(String[] args) {
-        System.out.println("test apliccation");
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Digite seu nome > ");
+		String name = in.nextLine();
+
+		System.out.println(name + ", kkkk");
+
+		in.close();
+	}
 }
